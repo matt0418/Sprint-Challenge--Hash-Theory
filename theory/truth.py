50d4f7439751721ac01a97ecae557daa5230da05
matt0418 <mfeldma7@gmail.com>
2019-06-21 12:19:56 -0400

    completed MVP for the dat

--- theory/truth.py
+++ theory/truth.py
@@ -1,3 +1,4 @@
 for A in [True, False]:
     for B in [True, False]:
-        print(f'{int(A)} -- {int(B)} -- {int(A or not B)}')
+        for C in [True, False]:
+                print(f'{int(A)} -- {int(B)} -- {int(C)} == {int(not(A or B) or ((A or C) and not(B or not C)))}')
